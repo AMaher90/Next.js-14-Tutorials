956d99f3e8b13777b69616925dbfcff59b551ba0
Ahmed Maher <ahmed.maher@publicissapient.com>
2024-05-10 16:56:06 +0400

    22 - Templates

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import "./styles.css";
+import { useState } from "react";
 
 const navLinks = [
   { name: "Register", href: "/register" },
@@ -15,15 +16,21 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   const pathName = usePathname();
+  const [input, setInput] = useState("");
+
   return (
     <div>
+      <div>
+        <input value={input} onChange={e => setInput(e.target.value)} />
+      </div>
       {navLinks.map((link) => {
-        const isActive = pathName.startsWith(link.href)
+        const isActive = pathName.startsWith(link.href);
         return (
-          <Link 
-          href={link.href} 
-          key={link.name}
-          className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}>
+          <Link
+            href={link.href}
+            key={link.name}
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+          >
             {link.name}
           </Link>
         );
